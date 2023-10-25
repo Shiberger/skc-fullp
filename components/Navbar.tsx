263e51cf8ee14fdc2ba4efa9312ac97fc6120c11
Shiberger <hannarong.tonkra@gmail.com>
2023-10-26 04:21:27 +0700

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,49 +1,43 @@
 import Image from "next/image";
 import Link from "next/link";
+
 import { NavLinks } from "@/constants";
-import AuthProviders from "./AuthProviders";
 import { getCurrentUser } from "@/lib/session";
 
+import AuthProviders from "./AuthProviders";
+import Button from "./Button";
+import ProfileMenu from "./ProfileMenu";
+
 const Navbar = async () => {
-  const session = await getCurrentUser();
+  const session = await getCurrentUser()
 
   return (
     <nav className='flexBetween navbar'>
       <div className='flex-1 flexStart gap-10'>
         <Link href='/'>
           <Image
-          src='/logo.svg'
-          width={116}
-          height={43}
-          alt='logo'
+            src='/logo.svg'
+            width={116}
+            height={43}
+            alt='logo'
           />
         </Link>
         <ul className='xl:flex hidden text-small gap-7'>
           {NavLinks.map((link) => (
-            <Link href={link.href} key={link.key}>
+            <Link href={link.href} key={link.text}>
               {link.text}
             </Link>
           ))}
         </ul>
       </div>
 
-      <div className="flexCenter gap-4">
+      <div className='flexCenter gap-4'>
         {session?.user ? (
           <>
-          {session?.user?.image && (
-          <Link href={`/profile/${session?.user?.id}`}>
-          <Image
-              src={session.user.image}
-              width={40}
-              height={40}
-              className="rounded-full"
-              alt={session.user.name}
-            />
-          </Link>
-        )}
+            <ProfileMenu session={session} />
 
             <Link href="/create-project">
-            Share Work
+              <Button title='Share work' />
             </Link>
           </>
         ) : (
@@ -51,7 +45,7 @@ const Navbar = async () => {
         )}
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
