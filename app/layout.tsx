3e191a6590a87d13b9ff3a09af9084364f6ee296
Shiberger <hannarong.tonkra@gmail.com>
2023-10-26 00:28:40 +0700

    update navbar.tsx / footer.tsx
    add footer

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import "./global.css";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+
+export const metadata = {
+    title: "Skc-full",
+    description: "Showcase and discover remarkable developer projects",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang='en'>
+            <body>
+                <Navbar />
+                <main>
+                    {children}
+                </main>
+                <Footer />
+            </body>
+        </html>
+    );
+}
