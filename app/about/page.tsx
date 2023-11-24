029194f1aed0ce3b9aba9f19e259587dc71b28f3
BamKS110 <bamkcn91@gmail.com>
2023-11-25 00:11:24 +0700

    clear about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,33 +1,23 @@
-import React from 'react'
-import HeroAbout from './HeroAbout'
-import HeroAbout2 from './HeroAbout2'
-import GridAbout from './GridAbout'
-import HeroAbout3 from './HeroAbout3'
-
-
-import ContentAbout from './ContentAbout'
-import CardAbout from './CardAbout'
-
-import ScrollToTop from '@/components/ScollToTop'
-import LastSection from './LastSection'
-
+import React from "react";
+import HeroAbout from "./HeroAbout";
+import HeroAbout2 from "./HeroAbout2";
+import GridAbout from "./GridAbout";
+import HeroAbout3 from "./HeroAbout3";
+import LastSection from "./LastSection";
+import ScrollToTop from "@/components/ScollToTop";
 
 
 function page() {
   return (
     <div>
-        <HeroAbout/>
-        <HeroAbout2/>
-        <GridAbout/>
-        <HeroAbout3/>
-        <LastSection/>
-        <ScrollToTop/>
-        
-
-
+      <HeroAbout />
+      <HeroAbout2 />
+      <GridAbout />
+      <HeroAbout3 />
+      <LastSection />
+      <ScrollToTop />
     </div>
-
-  )
+  );
 }
 
-export default page
+export default page;
